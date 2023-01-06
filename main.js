7fda838255cd7a7352d591fd079df0cc225af630
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2023-01-06 13:56:42 -0300

    06012022

--- main.js
+++ main.js
@@ -1,29 +1,17 @@
-<<<<<<< HEAD
-function tocaSom(){
-    document.querySelector().play();
-}
-
-=======
 function tocarSom (idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
->>>>>>> 709306399d23f0a601799efb5806cbe0e695df15
 let contador = 0
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 while( contador < listaDeTeclas.length){
 
-<<<<<<< HEAD
-    listaDeTeclas[contador].onclick = tocaSom
-    contador = contador + 1
-=======
+    listaDeTeclas[contador].onclick = tocarSom
+    
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
->>>>>>> 709306399d23f0a601799efb5806cbe0e695df15
 
     const idAudio = `#som_${instrumento}`;
 
