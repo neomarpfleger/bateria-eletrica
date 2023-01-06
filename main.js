d05227fc80acda4db4bcf12b8e336ade11535112
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2023-01-06 04:25:56 -0300

    06012023

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
+function tocarSom (){
+    document.querySelector('#som_tecla_pom').play();
+}
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
-listaDeTeclas[0].addEventListener("click", function(){
-    document.querySelector('#som_tecla_pom').play();
-})
 
 let contador = 0
 
 while( contador < listaDeTeclas.length){
+
+    listaDeTeclas[contador].onclick = function (){
+        tocarSom('')
+    }
 
     contador = contador + 1
 
